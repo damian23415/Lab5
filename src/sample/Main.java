4a422c9c93d8820b39350bb9cdaed84a70a9a321
Damian <damian.radomski9908@gmail.com>
2021-03-22 11:19:49 +0100

    Thirst task

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,7 +11,12 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.canvas.Canvas;
+import javafx.util.Duration;
+import jdk.jfr.Event;
+
 import java.awt.*;
+import java.security.Key;
+import java.util.EventListener;
 
 public class Main extends Application {
     private static final double WIDTH = 800;
@@ -22,6 +29,7 @@ public class Main extends Application {
     private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
     private static final double R = 10;
+    private double x = ARENAX1 + ARENAWIDTH / 2;
 
     @Override
     public void start(Stage stage) {
@@ -31,11 +39,28 @@ public class Main extends Application {
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
+//        gc.setFill(Color.BLACK);
+//        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+//
+//        gc.setFill((Color.WHITESMOKE));
+//        gc.fillOval(ARENAX1 + ARENAWIDTH / 2, ARENAY1 + ARENAHEIGHT / 2, 2 * R, 2 * R);
+
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+        t.setCycleCount(Timeline.INDEFINITE);
+        t.play();
+    }
+
+    @FunctionalInterface
+    public interface EventHandler<T extends Event> extends EventListener {
+        void handle(T event);
+    }
+
+    private void run(GraphicsContext gc) {
         gc.setFill(Color.BLACK);
         gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
 
         gc.setFill((Color.WHITESMOKE));
-        gc.fillOval(ARENAX1 + ARENAWIDTH / 2, ARENAY1 + ARENAHEIGHT / 2, 2 * R, 2 * R);
+        gc.fillOval(x += 5, ARENAY1 + ARENAHEIGHT / 2, 2 * R, 2 * R);
     }
 
 
