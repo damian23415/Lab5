4cd57ea4e7a81c60f2aca17393049f3b10b18f6d
Damian <damian.radomski9908@gmail.com>
2021-03-22 11:03:34 +0100

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import java.awt.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Canvas canvas = new Canvas(800, 600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        stage.setTitle("Kulki!");
+        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.show();
+
+        gc.setFill(Color.BLACK);
+        gc.fillRect(50, 50, 700, 500);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
